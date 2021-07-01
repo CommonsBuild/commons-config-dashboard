42a180f9c64cb7094b32a634e8bdbd92ab8f2148
Vitor Marthendal <marthendalnunes@gmail.com>
2021-07-01 18:04:54 -0300

    update app host and port

--- app.py
+++ app.py
@@ -15,6 +15,3 @@ app = dash.Dash(__name__,
                 external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[html.H1(children='Commons Configuration Dashboard')])
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
--- index.py
+++ index.py
@@ -2,4 +2,8 @@ from app import app, server
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(
+        host='127.0.0.1',
+        port=8085,
+        debug=True,
+    )
