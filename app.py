8cb510c149c258510665d230729f0bdc294cbf06
Vitor Marthendal <marthendalnunes@gmail.com>
2021-07-04 15:46:02 -0300

    add server to app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app = dash.Dash(__name__,
 )
 
 app.layout = html.Div(children=[html.H1(children='Commons Configuration Dashboard')])
+server = app.server
 
 if __name__ == "__main__":
     app.run_server(debug=True)
