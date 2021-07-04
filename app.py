a92949d42ed907b97ab7b24dd2da2e43c0e33fe8
Vitor Marthendal <marthendalnunes@gmail.com>
2021-07-04 15:30:40 -0300

    update app.py

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import flask
 import plotly.express as px
 import pandas as pd
-
-server = flask.Flask(__name__) # define flask app.server
 
 
 app = dash.Dash(__name__,
                 title="Commons Config Dash",
-                suppress_callback_exceptions=True,
-                )
+)
 
 app.layout = html.Div(children=[html.H1(children='Commons Configuration Dashboard')])
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
