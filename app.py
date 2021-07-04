f991bc2b9eca58da05f01eb2228c114dae6dbcdb
Vitor Marthendal <marthendalnunes@gmail.com>
2021-07-04 15:19:35 -0300

    update app run

--- app.py
+++ app.py
@@ -7,12 +7,16 @@ import pandas as pd
 
 server = flask.Flask(__name__) # define flask app.server
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__,
                 server=server,
                 title="Commons Config Dash",
                 suppress_callback_exceptions=True,
-                external_stylesheets=external_stylesheets)
+                #external_stylesheets=external_stylesheets
+                )
 
 app.layout = html.Div(children=[html.H1(children='Commons Configuration Dashboard')])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
