5082649ad6541a013e3d530d10ef9ea13649b34d
Vitor Marthendal <marthendalnunes@gmail.com>
2021-07-04 15:25:11 -0300

    delete index.py execution

--- app.py
+++ app.py
@@ -7,13 +7,10 @@ import pandas as pd
 
 server = flask.Flask(__name__) # define flask app.server
 
-#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__,
-                server=server,
                 title="Commons Config Dash",
                 suppress_callback_exceptions=True,
-                #external_stylesheets=external_stylesheets
                 )
 
 app.layout = html.Div(children=[html.H1(children='Commons Configuration Dashboard')])
--- index.py
+++
@@ -1,9 +0,0 @@
-from app import app, server
-
-
-if __name__ == "__main__":
-    app.run_server(
-        host='127.0.0.1',
-        port=8085,
-        debug=True,
-    )
