c5ddb3e1c8d0ca6439eee62adc9d059d5e4c5476
Vitor Marthendal <marthendalnunes@gmail.com>
2021-07-11 14:54:03 -0300

    Structure dashboard stages

--- layout/layout.py
+++ layout/layout.py
@@ -5,7 +5,7 @@ app_layout = html.Div([
     html.H1('Commons Configuration Dashboard'),
     html.Div([
         html.Div([
-            html.H2('4. Token Freeze and Token Thaw'),
+            html.H2('1. Token Freeze and Token Thaw'),
             html.Div([
                 html.Label('Opening Price (wxDai)'),
                 dcc.Input(
@@ -39,7 +39,16 @@ app_layout = html.Div([
                 dcc.Graph(id='token_freeze_thaw'),
             ], className='output')
             
-        ], className='output-row')
+        ], className='output-row'),
+        html.Div([
+            html.H2('2. The Augmented Bonding Curve (ABC)'),
+        ], className='input-row'),
+        html.Div([
+            html.H2('3. Disputable Voting'),
+        ], className='input-row'),
+        html.Div([
+            html.H2('4. Disputable Conviction Voting'),
+        ], className='input-row'),
               
     ])
 ])
